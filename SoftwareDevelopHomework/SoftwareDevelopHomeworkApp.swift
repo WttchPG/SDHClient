a93fc416ee3f437253b706492b383018bca3e78f
wttch <wttch@wttch.com>
2023-10-18 22:32:03 +0800

    Initial Commit

---
+++ SoftwareDevelopHomework/SoftwareDevelopHomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoftwareDevelopHomeworkApp.swift
+//  SoftwareDevelopHomework
+//
+//  Created by Wttch on 2023/10/18.
+//
+
+import SwiftUI
+
+@main
+struct SoftwareDevelopHomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
